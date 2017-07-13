62c199d85f0e0048a20ba17728c75826efeb3ce8
gabrielestrela <gabrielestrela92@gmail.com>
2017-07-13 04:04:17 +0000

    First commit

---
+++ JS/carousel.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    var slider = $('.carousel-intro').unslider({
+        autoplay: true,
+        nav: false,
+        arrows: false,
+        speed: 750,
+        delay: 7000,
+        animation: 'fade'
+    });
+
+    var windowHeight = $(window).height();
+    var liHeight = $(".carousel-inner li").height();
+    console.log('Window Height: ' + windowHeight);
+    if (windowHeight > liHeight) {
+            $('.carousel-inner').css({
+                height: liHeight
+            });
+        } else {
+            $('.carousel-inner').css({
+                height: windowHeight
+            });
+        }
+    //    $('.carousel-intro').css({
+    //        height: windowHeight
+    //    });
+
+    $(window).resize(function () {
+        var windowHeight = $(window).height();
+        var liHeight = $(".carousel-inner li").height();
+        console.log("Resize: " + windowHeight);
+        //        $('.carousel-intro').css({
+        //            height: windowHeight
+        //        });
+        if (windowHeight > liHeight) {
+            $('.carousel-inner').css({
+                height: liHeight
+            });
+        } else {
+            $('.carousel-inner').css({
+                height: windowHeight
+            });
+        }
+    });
+    //    var pPos = $('.my-slider').offset();
+    //    var wHeight = $(window).height();
+    //    var wWidth = $(window).width();
+    //    var top = pPos.top + 770;
+    //    var left = pPos.left + 1140;
+    //
+    //    slider.on('unslider.change', function (event, index, slide) {
+    //        if (index == 0) {
+    //            console.log("Slide has been changed to: " + 0);
+    //            $('.corner').css({
+    //                top: top,
+    //                left: left
+    //            });
+    //            $('$title1').addClass('show').removeClass('hide');
+    //            $('$title2').addClass('hide').removeClass('show');
+    //        }
+    //        if (index == 1) {
+    //            console.log("Slide has been changed to: " + 1);
+    //            $('.corner').css({
+    //                top: top,
+    //                left: left
+    //            });
+    //            $('$title1').addClass('hide').removeClass('hide');
+    //            $('$title2').addClass('hide').removeClass('show');
+    //        }
+    //    });
+});
---
+++ JS/nav.js
@@ -0,0 +1,127 @@
+$(document).ready(function () {
+    //    $('.brand-logo img').css("margin-top", "20px");
+    if ($(window).scrollTop() == 0) {
+        $('.button-collapse i').css({
+            "height": "128px",
+            "line-height": "128px"
+        });
+    }
+
+    if ($(window).scrollTop() != 0 && $(window).scrollTop() > 22) {
+        $('.button-collapse i').css({
+            "height": "64px",
+            "line-height": "64px"
+        });
+    }
+
+    //    function checkSize() {
+    //        if ($('#sizeCheck').css("padding") == "4px") {
+    //            $('#STICKY').addClass('navbar-fixed');
+    //            $('#STICKY nav').removeClass('FLOAT');
+    //            $('.brand-logo img').attr("src", "IMAGES/logos/logo128.png");
+    //            $('.brand-logo img').css("margin-top", "6px");
+    //            $('.button-collapse i').css({
+    //                "height": "64px",
+    //                "line-height": "64px"
+    //            });
+    //            $('nav').css({
+    //                "height": "64px",
+    //                "line-height": "64px",
+    //                "top": "0"
+    //            }, 300);
+    //        } else {
+    //            $(window).scroll(function () {
+    //                console.log('Window-ScrollTop: ' + $(window).scrollTop());
+    //
+    //                if ($(window).scrollTop() > 22) {
+    //                    $('.button-collapse i').css({
+    //                        "height": "64px",
+    //                        "line-height": "64px"
+    //                    });
+    //                    $('nav').css({
+    //                        "height": "64px",
+    //                        "line-height": "64px",
+    //                        "top": "0"
+    //                    }, 300);
+    //                    $('#STICKY').addClass('navbar-fixed');
+    //                    $('#STICKY nav').removeClass('FLOAT');
+    //                    $('.brand-logo img').attr("src", "IMAGES/logos/logo128.png");
+    //                    $('.brand-logo img').css("margin-top", "6px");
+    //                } else {
+    //                    $('.button-collapse i').css({
+    //                        "height": "128px",
+    //                        "line-height": "128px"
+    //                    });
+    //                    $('nav').css({
+    //                        "height": "128px",
+    //                        "line-height": "128px",
+    //                        "top": "25px"
+    //                    }, 300);
+    //                    $('#STICKY nav').addClass('FLOAT');
+    //                    $('#STICKY').removeClass('navbar-fixed');
+    //                    $('.brand-logo img').attr("src", "IMAGES/logos/logo256.png");
+    //                    $('.brand-logo img').css("margin-top", "20px");
+    //                }
+    //
+    //            });
+    //        }
+    //    }
+    //
+    //    $(window).resize(checkSize);
+    function fixBar() {
+        $('.button-collapse i').css({
+            "height": "64px",
+            "line-height": "64px"
+        });
+        $('nav').css({
+            "height": "64px",
+            "line-height": "64px",
+            "top": "0"
+        }, 300);
+        $('#STICKY').addClass('navbar-fixed');
+        $('#STICKY nav').removeClass('FLOAT');
+        $('.brand-logo img').attr("src", "IMAGES/logos/logo128.png");
+        $('.brand-logo img').css("margin-top", "6px");
+    }
+
+    function floatBar() {
+        $('.button-collapse i').css({
+            "height": "128px",
+            "line-height": "128px"
+        });
+        $('nav').css({
+            "height": "128px",
+            "line-height": "128px",
+            "top": "25px"
+        }, 300);
+        $('#STICKY nav').addClass('FLOAT');
+        $('#STICKY').removeClass('navbar-fixed');
+        $('.brand-logo img').attr("src", "IMAGES/logos/logo256.png");
+        $('.brand-logo img').css("margin-top", "20px");
+    }
+
+    function sizeCheck() {
+        if ($("#sizeCheck").css("padding") != "4px") {
+//            console.log("Tela Maior");
+            $(window).scroll(function () {
+                console.log('Window-ScrollTop: ' + $(window).scrollTop() + "\r");
+
+                if ($(window).scrollTop() > 22) {
+                    fixBar();
+                }
+                if ($(window).scrollTop() <= 22 && $("#sizeCheck").css("padding") != "4px") {
+                    floatBar();
+                }
+
+            });
+        } else {
+//            console.log("Tela Menor");
+            fixBar();
+        }
+    }
+
+    sizeCheck();
+
+    $(window).resize(sizeCheck);
+
+});
